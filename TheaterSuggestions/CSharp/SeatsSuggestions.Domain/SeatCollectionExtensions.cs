21614a76dc516168098cd7821765cc0934ddaf12
Bruno Boucard <boucard.bruno@free.fr>
2019-06-17 21:15:01 +0200

    Refactoring SelectAdjacentSeats

--- TheaterSuggestions/CSharp/SeatsSuggestions.Domain/SeatCollectionExtensions.cs
+++ TheaterSuggestions/CSharp/SeatsSuggestions.Domain/SeatCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,14 +13,16 @@ namespace SeatsSuggestions.Domain
             return seats.Where(s => s.IsAvailable() && s.MatchCategory(pricingCategory));
         }
 
-        public static IEnumerable<AdjacentSeats> SelectAdjacentSeats(this IEnumerable<Seat> seats, int partyRequested)
+        public static IEnumerable<AdjacentSeats> SelectAdjacentSeats(this IEnumerable<Seat> seats,
+            int partySize)
         {
             var adjacentSeatsGroups = new List<AdjacentSeats>();
             var adjacentSeats = new List<Seat>();
+            var currentSeats = seats.ToList();
 
-            if (partyRequested == 1) return seats.Select(s => new AdjacentSeats(new List<Seat> { s }));
+            if (partySize == 1) return currentSeats.Select(s => new AdjacentSeats(new List<Seat> {s}));
 
-            foreach (var candidateSeat in seats.OrderBy(s => s.DistanceFromRowCentroid))
+            foreach (var candidateSeat in currentSeats.OrderBy(s => s.DistanceFromRowCentroid).ToList())
             {
                 if (!adjacentSeats.Any())
                 {
@@ -29,18 +32,29 @@ namespace SeatsSuggestions.Domain
 
                 adjacentSeats = adjacentSeats.OrderBy(s => s.Number).ToList();
 
-                if (DoesNotExceedPartyRequestedAndCandidateSeatIsAdjacent(candidateSeat, adjacentSeats, partyRequested))
+                if (DoesNotExceedPartyRequestedAndCandidateSeatIsAdjacent(candidateSeat, adjacentSeats, partySize))
                 {
                     adjacentSeats.Add(candidateSeat);
+
+                    if (NoMoreSeats(adjacentSeats, currentSeats))
+                    {
+                        adjacentSeatsGroups.Add(new AdjacentSeats(adjacentSeats));
+                    }
                 }
                 else
                 {
                     if (adjacentSeats.Any()) adjacentSeatsGroups.Add(new AdjacentSeats(adjacentSeats));
-                    adjacentSeats = new List<Seat> { candidateSeat };
+
+                    adjacentSeats = new List<Seat> {candidateSeat};
                 }
             }
 
-            return adjacentSeatsGroups.Where(adjacent => adjacent.Count() == partyRequested);
+            return adjacentSeatsGroups.Where(adjacent => adjacent.Count() == partySize);
+        }
+
+        private static bool NoMoreSeats(ICollection adjacentSeats, ICollection currentSeats)
+        {
+            return adjacentSeats.Count == currentSeats.Count;
         }
 
         private static bool DoesNotExceedPartyRequestedAndCandidateSeatIsAdjacent(Seat candidateSeat,
